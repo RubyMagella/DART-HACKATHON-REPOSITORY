47b1d09f2a4a98b2e10d0bbf7e61003d71843f4d
Ruby Itimu Magella <160042683+RubyMagella@users.noreply.github.com>
2024-03-18 17:58:50 +0300

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -1,2 +1,10 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+void main(){
+  //Function to perform addition
+  int addTwo(int a,int b)
+  {return a+ b;}
+  print(addTwo(2,3));
+
+  //Function to perform subtraction
+  int subtractTwo(int a,int b)
+  {return a - b;}
+  print(subtractTwo(40,7));
